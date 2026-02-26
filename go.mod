e10ea92aa6cbaecebef5f44c661fc607d7e59980
Alshazly Albadry <alshazlem3@gmail.com>
2026-02-26 16:36:20 +0200

    initail commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module agi
+
+go 1.25.6
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/wenerme/astgo v0.0.0-20230926205800-1b5bc38663fa // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/wenerme/astgo/agi"
+
+func main() {
+	handler := (func(session *agi.Session) {
+		client := session.Client()
+		client.Answer()
+		client.StreamFile("tt-monkeys", "#")
+		client.Exec("Dial", "PJSIP/PHONE_B")
+		client.SetVariable("AGISTATUS", "SUCCESS")
+		client.Hangup()
+	})
+
+	err := agi.Listen(":4573", handler)
+	if err != nil {
+		panic(err)
+	}
+}
